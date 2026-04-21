fbec9dd2e63bebd4654c2cf9278908998da17fed
maybe-adamant <maybe.adamant@gmail.com>
2026-04-21 15:09:39 -0400

    Late-bind framework callbacks on reload

--- src/main.lua
+++ src/main.lua
@@ -17,8 +17,6 @@ local reload  = mods['SGG_Modding-ReLoad']
 
 local config = chalk.auto('config.lua')
 
-local Framework = mods['adamant-ModpackFramework']
-
 local def = {
     NUM_PROFILES    = #config.Profiles,
     defaultProfiles = {},
@@ -30,8 +28,37 @@ local def = {
 }
 
 local PACK_ID = "run-director"
+local frameworkCallbackCache = {}
+
+local function GetFramework()
+    return rom.mods["adamant-ModpackFramework"]
+end
+
+local function GetFrameworkCallback(factoryName)
+    local framework = GetFramework()
+    local factory = framework and framework[factoryName]
+    if type(factory) ~= "function" then
+        frameworkCallbackCache[factoryName] = nil
+        return nil
+    end
+
+    local cached = frameworkCallbackCache[factoryName]
+    if not cached or cached.framework ~= framework or cached.factory ~= factory then
+        cached = {
+            framework = framework,
+            factory = factory,
+            callback = factory(PACK_ID),
+        }
+        frameworkCallbackCache[factoryName] = cached
+    end
+    return cached.callback
+end
 
 local function init()
+    local Framework = GetFramework()
+    assert(Framework and type(Framework.init) == "function",
+        "adamant-RunDirector_Core: adamant-ModpackFramework is not loaded")
+
     Framework.init({
         packId      = PACK_ID,
         windowTitle = "Run Director",
@@ -42,10 +69,31 @@ local function init()
     })
 end
 
+local function renderWindow()
+    local callback = GetFrameworkCallback("getRenderer")
+    if callback then
+        callback()
+    end
+end
+
+local function alwaysDraw()
+    local callback = GetFrameworkCallback("getAlwaysDrawRenderer")
+    if callback then
+        callback()
+    end
+end
+
+local function addMenuBar()
+    local callback = GetFrameworkCallback("getMenuBar")
+    if callback then
+        callback()
+    end
+end
+
 local loader = reload.auto_single()
 modutil.once_loaded.game(function()
-    rom.gui.add_imgui(Framework.getRenderer(PACK_ID))
-    rom.gui.add_always_draw_imgui(Framework.getAlwaysDrawRenderer(PACK_ID))
-    rom.gui.add_to_menu_bar(Framework.getMenuBar(PACK_ID))
-    loader.load(init)
+    rom.gui.add_imgui(renderWindow)
+    rom.gui.add_always_draw_imgui(alwaysDraw)
+    rom.gui.add_to_menu_bar(addMenuBar)
+    loader.load(nil, init)
 end)
