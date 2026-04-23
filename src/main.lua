2472091b204a99038269f9402d1e369a96237540
maybe-adamant <maybe.adamant@gmail.com>
2026-04-23 17:11:31 -0400

    safer imgui attachment

--- src/main.lua
+++ src/main.lua
@@ -44,7 +44,8 @@ local function init()
 end
 
 local loader = reload.auto_single()
-modutil.once_loaded.game(function()
+
+local function registerGui()
     local Framework = rom.mods["adamant-ModpackFramework"]
     assert(Framework and type(Framework.getRenderer) == "function",
         "adamant-RunDirector_Core: adamant-ModpackFramework is not loaded")
@@ -52,5 +53,8 @@ modutil.once_loaded.game(function()
     rom.gui.add_imgui(Framework.getRenderer(PACK_ID))
     rom.gui.add_always_draw_imgui(Framework.getAlwaysDrawRenderer(PACK_ID))
     rom.gui.add_to_menu_bar(Framework.getMenuBar(PACK_ID))
-    loader.load(nil, init)
+end
+
+modutil.once_loaded.game(function()
+    loader.load(registerGui, init)
 end)
